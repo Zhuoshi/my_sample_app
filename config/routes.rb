eaba30b5a3d13316b6910a4d7543dafcba65c749
Zhuoshi Wei <zhuoshiwei@gmail.com>
2012-07-11 22:54:44 -0400

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 SampleApp::Application.routes.draw do
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   #get "users/new"
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
   #get "static_pages/home"
 
   root to: 'static_pages#home'
